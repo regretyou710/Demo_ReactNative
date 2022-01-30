ce314f6c7d8af54770a85a24e52ae9b02c6c09eb
regretyou710 <regretyou710@gmail.com>
2022-01-30 12:29:43 +0800

    202201301229

---
+++ demo063/RN01/App.js
@@ -0,0 +1,28 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+// eslint-disable-next-line prettier/prettier
+import React, {
+  Component
+} from 'react';
+
+// eslint-disable-next-line prettier/prettier
+import {
+  View,
+  Text
+} from 'react-native';
+
+export default class App extends Component<{}> {
+  render() {
+    return (
+      <View>
+        <Text>文字內容測試</Text>
+      </View>
+    );
+  }
+};
